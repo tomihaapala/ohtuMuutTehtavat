2648e36c338fac327a3e59dc539a5ce4a8c5a33c
tomihaapala <tomi.haapala@gmail.com>
2021-03-12 21:56:27 +0200

    selenium

--- koodi/viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ koodi/viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -8,12 +8,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Tester {
 
     public static void main(String[] args) {
+
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tomi\\Documents\\chromedriver");
         WebDriver driver = new ChromeDriver();
 
         driver.get("http://localhost:4567");
-        
+
         sleep(2);
-        
+        /*
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
@@ -22,20 +24,44 @@ public class Tester {
         element = driver.findElement(By.name("username"));
         element.sendKeys("pekka");
         element = driver.findElement(By.name("password"));
-        element.sendKeys("akkep");
+        element.sendKeys("axkkep");
         element = driver.findElement(By.name("login"));
         
-        sleep(2);
+         sleep(2);
         element.submit();
+         */
+        // toka
+        WebElement element2 = driver.findElement(By.linkText("register new user"));
+        element2.click();
 
-        sleep(3);
-        
+        sleep(2);
+
+        element2 = driver.findElement(By.name("username"));
+        element2.sendKeys("pekka622222");
+        element2 = driver.findElement(By.name("password"));
+        element2.sendKeys("axkkep3");
+        element2 = driver.findElement(By.name("passwordConfirmation"));
+        element2.sendKeys("axkkep3");
+        element2 = driver.findElement(By.name("signup"));
+        element2.click();
+        sleep(2);
+        element2 = driver.findElement(By.linkText("continue to application mainpage"));
+        element2.click();
+        sleep(2);
+        element2 = driver.findElement(By.linkText("logout"));
+        element2.click();
+        sleep(2);
+        //element2.submit();
+
+       // sleep(3);
+
         driver.quit();
     }
-    
-    private static void sleep(int n){
-        try{
-            Thread.sleep(n*1000);
-        } catch(Exception e){}
+
+    private static void sleep(int n) {
+        try {
+            Thread.sleep(n * 1000);
+        } catch (Exception e) {
+        }
     }
 }
