f77a7cc46171e9e5fbf5f62772c151a3fee0d807
tomihaapala <tomi.haapala@gmail.com>
2021-03-12 09:42:29 +0200

    nhl

--- koodi/viikko3/nhlreader/src/main/java/ohtu/Main.java
+++ koodi/viikko3/nhlreader/src/main/java/ohtu/Main.java
@@ -5,10 +5,43 @@
  */
 package ohtu;
 
-/**
- *
- * @author mluukkai
- */
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import org.apache.http.client.fluent.Request;
+
 public class Main {
-    
+
+    public static void main(String[] args) throws IOException {
+        String url = "https://nhlstatisticsforohtu.herokuapp.com/players";
+
+        String bodyText = Request.Get(url).execute().returnContent().asString();
+
+        System.out.println("json-muotoinen data:");
+        System.out.println(bodyText);
+
+        Gson mapper = new Gson();
+        Player[] players = mapper.fromJson(bodyText, Player[].class);
+        ArrayList list = new ArrayList<Player>();
+
+        System.out.println("Oliot:");
+
+        for (Player player : players) {
+            if (player.getNationality().equals("FIN")) {
+                list.add(player);
+            }
+        }
+
+        Collections.sort(list);
+
+        int i = 0;
+        while (i <= list.size()-1) {
+            System.out.println(list.get(i));
+            i++;
+        }
+    }
+
 }
--- koodi/viikko3/nhlreader/src/main/java/ohtu/Player.java
+++ koodi/viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -1,8 +1,69 @@
-
 package ohtu;
 
-public class Player {
+public class Player implements Comparable <Player>{
+
     private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private String team;
+    private int games;
+    private int tehopisteet;
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+    public void setTehopisteet(){
+        this.tehopisteet = goals + assists;
+    }
+    public int getTehopisteet(){
+        return this.tehopisteet;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -13,8 +74,16 @@ public class Player {
     }
 
     @Override
-    public String toString() {
-        return name;
+    public int compareTo(Player player) {
+        setTehopisteet();
+        int compareAssists = ((Player) player).getTehopisteet();
+        return  compareAssists - this.tehopisteet;
     }
-      
+
+    @Override
+    public String toString() {
+        //Henrik Borgstrom team FLA goals 0 assists 0
+        return name + " team " + team + " goals " + goals + " assists " + assists + " tehopisteet " + tehopisteet;
+    }
+
 }
