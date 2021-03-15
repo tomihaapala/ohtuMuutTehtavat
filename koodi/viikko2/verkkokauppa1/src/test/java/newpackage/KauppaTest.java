c4583dc6402bb23e27386f54ea9aeb8ab4a5cfee
tomihaapala <tomi.haapala@gmail.com>
2021-03-15 21:51:44 +0200

    verkkokauppa jacoco

--- koodi/viikko2/verkkokauppa1/src/test/java/newpackage/KauppaTest.java
+++ koodi/viikko2/verkkokauppa1/src/test/java/newpackage/KauppaTest.java
@@ -1,14 +1,19 @@
+package newpackage;
+
 
 import ohtu.verkkokauppa.Kauppa;
 import ohtu.verkkokauppa.Pankki;
 import ohtu.verkkokauppa.Tuote;
 import ohtu.verkkokauppa.Varasto;
 import ohtu.verkkokauppa.Viitegeneraattori;
+import ohtu.verkkokauppa.rajapinnat.PankkiIO;
+import ohtu.verkkokauppa.rajapinnat.ViitegeneraattoriIO;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -33,23 +38,23 @@ public class KauppaTest {
 
         pankki = mock(Pankki.class);
 
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        viitegeneraattori = mock(Viitegeneraattori.class);
 
-        when(viite.uusi()).thenReturn(42);
+        when(viitegeneraattori.uusi()).thenReturn(42);
 
         varasto = mock(Varasto.class);
 
         // m‰‰ritell‰‰n ett‰ tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10);
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-        
+
         when(varasto.saldo(2)).thenReturn(1);
         when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 5));
-        
+
         when(varasto.saldo(3)).thenReturn(0);
         when(varasto.haeTuote(3)).thenReturn(new Tuote(3, "pulla", 2));
 
-        kauppa = new Kauppa(varasto, pankki, viite);
+        kauppa = new Kauppa(varasto, pankki, viitegeneraattori);
     }
 
     @Test
@@ -80,22 +85,46 @@ public class KauppaTest {
 
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);  // ostetaan tuotetta numero 1 eli maitoa
-        kauppa.lisaaKoriin(2); 
+        kauppa.lisaaKoriin(2);
         kauppa.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, ett‰ pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 10);
     }
-    
+
     @Test
     public void tehdaanKaksiOstostaJoistaToinenLoppu() {
 
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);  // ostetaan tuotetta numero 1 eli maitoa
-        kauppa.lisaaKoriin(3); 
+        kauppa.lisaaKoriin(3);
         kauppa.tilimaksu("pekka", "12345");
 
         // sitten suoritetaan varmistus, ett‰ pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 5);
+    }
+
+    @Test
+    public void aloitaAsiointiTyhjaaKorin() {
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);
+        kauppa.lisaaKoriin(2);
+        kauppa.aloitaAsiointi();
+        kauppa.tilimaksu("pekka", "12345");
+        verify(pankki).tilisiirto("pekka", 42, "12345", "33333-44455", 0);
+    }
+
+    @Test
+    public void eriViitteet() {
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);  // ostetaan tuotetta numero 1 eli maitoa
+        kauppa.tilimaksu("pekka", "12345");
+        verify(viitegeneraattori, times(1)).uusi();
+
+        kauppa.aloitaAsiointi();
+        kauppa.lisaaKoriin(1);  // ostetaan tuotetta numero 1 eli maitoa
+        kauppa.tilimaksu("pekka", "12345");
+        verify(viitegeneraattori, times(2)).uusi();
     }
 }
