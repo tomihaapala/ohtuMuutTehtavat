0544465fb717dd0207dbfa326587ab5ac5264d60
tomihaapala <tomi.haapala@gmail.com>
2021-03-22 11:07:52 +0200

    ksp

--- koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
+++ koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/Paaohjelma.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Paaohjelma {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static KiviPaperiSakset uusiPeli;
 
     public static void main(String[] args) {
 
@@ -16,22 +17,10 @@ public class Paaohjelma {
                     + "\nmuilla valinnoilla lopetataan");
 
             String vastaus = scanner.nextLine();
-            if (vastaus.endsWith("a")) {
-                System.out.println("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
-                KPSPelaajaVsPelaaja kaksinpeli = new KPSPelaajaVsPelaaja();
-                kaksinpeli.pelaa();
-            } else if (vastaus.endsWith("b")) {
-                System.out.println("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
-                KPSTekoaly yksinpeli = new KPSTekoaly();
-                yksinpeli.pelaa();
-            } else if (vastaus.endsWith("c")) {
-                System.out.println("peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s");
-                KPSParempiTekoaly pahaYksinpeli = new KPSParempiTekoaly();
-                pahaYksinpeli.pelaa();
-            } else {
-                break;
+             if (vastaus.endsWith("a")) {
+                uusiPeli = KiviPaperiSakset.luoUusiPelaajaVsPelaaja();
             }
-
+             uusiPeli.pelaa();
         }
 
     }
