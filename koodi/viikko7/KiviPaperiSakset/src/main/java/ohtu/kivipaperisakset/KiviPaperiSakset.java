232bb25153b4466033b3577e07342d3ec4e85115
tomihaapala <48236861+tomihaapala@users.noreply.github.com>
2021-03-22 13:13:11 +0200

    Update KiviPaperiSakset.java

--- koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KiviPaperiSakset.java
+++ koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KiviPaperiSakset.java
@@ -11,51 +11,58 @@ import java.util.Scanner;
  *
  * @author Tomi
  */
-public abstract class KiviPaperiSakset {
+public abstract class KiviPaperiSakset{
+
     static final Scanner scanner = new Scanner(System.in);
+    //public String ekanSiirto;
 
     static KiviPaperiSakset luoUusiPelaajaVsPelaaja() {
-       return new KPSPelaajaVsPelaaja();
+        return new KPSPelaajaVsPelaaja();
     }
-    
-    
+
+    static KiviPaperiSakset luoUusiPelaajaVsTekoaly() {
+        return new KPSTekoaly();
+    }
+
+    static KiviPaperiSakset luoUusiPelaajaVsVahvaTekoaly() {
+        return new KPSParempiTekoaly();
+    }
+
     // tämä on ns template metodi
     public void pelaa() {
-        Tuomari tuomari = new Tuomari() ;
-        
-        String ekanSiirto = scanner.nextLine();
-        
-        
-        System.out.print("Toisen pelaajan siirto: ");
-        String tokanSiirto = toisenSiirto();
-        
+        Tuomari tuomari = new Tuomari();
+
+        System.out.println("Anna eka:");
+        String ekanSiirto = ensimmaisenSiirto();
+
+        System.out.println("Toisen pelaajan siirto: ");
+        String tokanSiirto = toisenSiirto(ekanSiirto);
+
         while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
             tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
             System.out.println(tuomari);
             System.out.println();
 
             System.out.println("Ensimmäisen pelaajan siirto: ");
-            ekanSiirto = scanner.nextLine();
-            
+            ekanSiirto = ensimmaisenSiirto();
+
             System.out.println("Toisen pelaajan siirto: ");
-            tokanSiirto = toisenSiirto();
-            
-           // tekoaly.asetaSiirto(ekanSiirto);
+            tokanSiirto = toisenSiirto(ekanSiirto);
+
         }
 
         System.out.println();
         System.out.println("Kiitos!");
         System.out.println(tuomari);
     }
-    
+
     protected String ensimmaisenSiirto() {
-        System.out.print("Ensimmäisen pelaajan siirto: ");
         return scanner.nextLine();
     }
 
     // tämä on abstrakti metodi sillä sen toteutus vaihtelee eri pelityypeissä
-    abstract protected String toisenSiirto();
-    
+    abstract protected String toisenSiirto(String ekanSiirto);
+
     protected static boolean onkoOkSiirto(String siirto) {
         return "k".equals(siirto) || "p".equals(siirto) || "s".equals(siirto);
     }
