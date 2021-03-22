cf480a4495dd1273b77a682db2944d655442c5ac
tomihaapala <48236861+tomihaapala@users.noreply.github.com>
2021-03-22 13:12:32 +0200

    Update KPSTekoaly.java

--- koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
+++ koodi/viikko7/KiviPaperiSakset/src/main/java/ohtu/kivipaperisakset/KPSTekoaly.java
@@ -2,42 +2,15 @@ package ohtu.kivipaperisakset;
 
 import java.util.Scanner;
 
-public class KPSTekoaly {
+public class KPSTekoaly extends KiviPaperiSakset {
 
     private static final Scanner scanner = new Scanner(System.in);
+    Tekoaly tekoaly = new Tekoaly();
 
-    public void pelaa() {
-        Tuomari tuomari = new Tuomari();
-        Tekoaly tekoaly = new Tekoaly();
-
-        System.out.println("Ensimmäisen pelaajan siirto: ");
-        String ekanSiirto = scanner.nextLine();
-        String tokanSiirto;
-
-        tokanSiirto = tekoaly.annaSiirto();
-        System.out.println("Tietokone valitsi: " + tokanSiirto);
-
-
-        while (onkoOkSiirto(ekanSiirto) && onkoOkSiirto(tokanSiirto)) {
-            tuomari.kirjaaSiirto(ekanSiirto, tokanSiirto);
-            System.out.println(tuomari);
-            System.out.println();
-
-            System.out.println("Ensimmäisen pelaajan siirto: ");
-            ekanSiirto = scanner.nextLine();
-
-            tokanSiirto = tekoaly.annaSiirto();
-            System.out.println("Tietokone valitsi: " + tokanSiirto);
-            tekoaly.asetaSiirto(ekanSiirto);
-
-        }
-
-        System.out.println();
-        System.out.println("Kiitos!");
-        System.out.println(tuomari);
-    }
-
-    private static boolean onkoOkSiirto(String siirto) {
-        return "k".equals(siirto) || "p".equals(siirto) || "s".equals(siirto);
+    @Override
+    protected String toisenSiirto(String edellinen) {
+        String siirto = tekoaly.annaSiirto();
+        System.out.println("Tekoäly valitsi: " + siirto);
+        return siirto;
     }
 }
